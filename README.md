8a63569638d88bb693435d1a727cc9cd59f19ade
David Margalit <david@margalit.io>
2023-04-01 01:50:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Flask Chatbot with GPT-4
+
+This is a Flask-based chatbot application that uses OpenAI's GPT-4 model for generating responses. The application uses Flask-SocketIO for real-time communication between the server and the client.
+
+## Environment Variables
+
+- `SYSTEM_CONTENT`: Configuration of the server, e.g. "Act as a helpful bot" (Optional)
+- `TEMPERATURE`: Controls the randomness of the model's output. Higher values (e.g., 2) make the output more random, while lower values (e.g., 0) make it more deterministic. Default is 0.75.
+- `MODEL`: The name of the OpenAI model to use. Default is "gpt-4".
+- `OPENAI_API_KEY`: Your OpenAI API key, which can be obtained from the OpenAI website.
+
+## Docker Images
+
+Docker images for this application are available on the [releases page](https://github.com/david1155/estherbot/releases).
+
+## Running the Application with Docker
+
+1. Pull the Docker image:
+
+   ```
+   docker pull david1155/estherbot:latest
+   ```
+
+2. Run the Docker container:
+
+   ```
+   docker run -d -p 5000:5000 -e OPENAI_API_KEY=your_api_key -e SYSTEM_CONTENT="Act as a helpful chatbot" -e TEMPERATURE=0 -e MODEL="gpt-3.5-turbo" david1155/estherbot:latest
+   ```
+
+   Replace `your_api_key` with your OpenAI API key, and adjust other environment variables as needed.
+
+3. Access the application in your browser at `http://localhost:5000`.
+
+## Obtaining an OpenAI API Key
+
+To obtain an API key for GPT-4, visit the [OpenAI website](https://platform.openai.com/) and sign up for an account. Once you have an account, you can access your API key from the dashboard.
---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+import json
+import sseclient
+import os
+import threading
+from flask_socketio import SocketIO, emit
+import uuid
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+if os.getenv('SYSTEM_CONTENT'):
+    syscont = os.getenv('SYSTEM_CONTENT')
+else:
+    syscont = ""
+
+if os.getenv('TEMPERATURE'):
+    temperature = os.getenv('TEMPERATURE')
+else:
+    temperature = 0.75
+
+if os.getenv('MODEL'):
+    model = os.getenv('MODEL')
+else:
+    model = "gpt-4"
+
+messages = [
+    {"role": "system", "content": syscont},
+]
+
+latest_user_input_id = None
+
+
+def performRequestWithStreaming(user_input, user_input_id):
+    global latest_user_input_id
+    messages.append({"role": "user", "content": user_input})
+    reqUrl = 'https://api.openai.com/v1/chat/completions'
+    reqHeaders = {
+        'Accept': 'text/event-stream',
+        'Authorization': 'Bearer ' + os.environ['OPENAI_API_KEY']
+    }
+    reqBody = {
+        "model": model,
+        "messages": messages,
+        "temperature": temperature,
+        "stream": True,
+    }
+    request = requests.post(reqUrl, stream=True,
+                            headers=reqHeaders, json=reqBody)
+    client = sseclient.SSEClient(request)
+    response = ""
+    for event in client.events():
+        if (
+            event.data != '[DONE]'
+            and 'content' in json.loads(event.data)['choices'][0]['delta']
+        ):
+            content = json.loads(event.data)[
+                'choices'][0]['delta']['content']
+            response += content
+            if latest_user_input_id != user_input_id:
+                break
+            socketio.emit('assistant_response', {'content': content})
+    messages.append({"role": "assistant", "content": response})
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    global latest_user_input_id
+    user_input = request.form['user_input']
+    user_input_id = uuid.uuid4()
+    latest_user_input_id = user_input_id
+    threading.Thread(target=performRequestWithStreaming,
+                     args=(user_input, user_input_id)).start()
+    return jsonify({'status': 'success'})
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+else:
+    app = app
